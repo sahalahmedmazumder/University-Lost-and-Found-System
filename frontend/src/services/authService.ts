c909dded9a34302dc2fe0fa8fe9b65f5ed984fd0
sahalahmedmazumder <sahalahmedmazumder@gmail.com>
2026-08-13 02:05:25 +0600

    Fix deployed API connection

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -7,7 +7,7 @@ import type {
     User,
 } from "../types/User";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "import.meta.env.VITE_API_URL";
 
 /* =========================
    REGISTER
