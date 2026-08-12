d7d68b6a08f56a1c9bbd88a8dac2de10bd39bf63
sahalahmedmazumder <sahalahmedmazumder@gmail.com>
2026-08-12 16:28:41 +0600

    Prepare backend for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,29 @@
-from app.routers.lost_item import router as lost_item_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers.health import router as health_router
 from app.routers.auth import router as auth_router
+from app.routers.lost_item import router as lost_item_router
 from app.routers.browse_items import router as browse_router
 from app.routers.found_item import router as found_item_router
 from app.routers.my_reports import router as my_reports_router
 from app.routers.admin import router as admin_router
 
+
 app = FastAPI()
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(health_router)
 app.include_router(auth_router)
@@ -26,6 +33,9 @@ app.include_router(found_item_router)
 app.include_router(my_reports_router)
 app.include_router(admin_router)
 
+
 @app.get("/")
 def root():
-    return {"message": "Backend running"}
+    return {
+        "message": "Backend running"
+    }
