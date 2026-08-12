cfc0bb8fbd084b5a426ddadf833c4f3cf099b8a0
sahalahmedmazumder <sahalahmedmazumder@gmail.com>
2026-08-13 01:43:50 +0600

    Configure CORS for Vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://university-lost-and-found-system.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
